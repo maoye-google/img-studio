b3b121986876f868ce09512296120b0748373486
maoye <maoye@google.com>
2025-01-23 10:40:04 +0000

    fix iap binding issue

--- terraform/oauth.tf
+++ terraform/oauth.tf
@@ -9,7 +9,7 @@ resource "google_iap_brand" "iap_oauth_app" {
 
   application_title = "(IAP protected) {ver.app_name}"
 
-  deletion_policy = "DELETE"
+  # deletion_policy = "DELETE"
 }
 
 # Create OAuth Client ID
@@ -18,15 +18,34 @@ resource "google_iap_client" "iap_oauth_client" {
   display_name = "IAP OAuth Client"
 }
 
+resource "google_project_service_identity" "iap_sa" {
+  provider = google-beta
+  project = var.project_id
+  service = "iap.googleapis.com"
+}
+
+# Grant the Permission to IAP's default Service Account
+# resource "google_project_iam_member" "iap_user" {
+#  project = var.project_id
+#  role    = "roles/roles/iap.httpsResourceAccessor"
+#  member  = google_project_service_identity.iap_sa.member
+# }
+
+# resource "google_project_iam_member" "run_invoker" {
+#  project = var.project_id
+#  role    = "roles/run.invoker"
+#  member  = google_project_service_identity.iap_sa.member
+# }
+
 resource "google_iap_web_backend_service_iam_binding" "iap_binding" {
   project            = var.project_id
   web_backend_service = google_compute_backend_service.default.name
   role               = "roles/iap.httpsResourceAccessor"
 
-  # members = [
-  #  "user:test-user@example.com",
-  #  "group:iap-access@example.com",
-  # ]
+  members = [
+    # "user:admin@maoye.altostrat.com",
+    google_project_service_identity.iap_sa.member
+  ]
 }
 
 
